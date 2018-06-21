18d89dcbad871c14686fd2d5d21f34a63e1c404a
James Stephenson <jsteph_fl@hotmail.com>
2018-06-20 21:06:52 -0400

    Lecture 120

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -10,3 +10,20 @@ ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank> (GetPawn());
 }
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Playercontroller begin play"));
+
+	ATank* tank = GetControlledTank();
+
+	if (tank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("We have a tank named: %s"), *tank->GetName());
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("no tank"));
+	}
+
+}
